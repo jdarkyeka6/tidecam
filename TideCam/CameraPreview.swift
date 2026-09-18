0140a3ced5f5bf0c00688579debb605c54b9cf96
Jake D <jakedryka1@gmail.com>
2026-09-18 14:47:03 +0800

    Add pinch gesture to camera preview

--- TideCam/CameraPreview.swift
+++ TideCam/CameraPreview.swift
@@ -4,6 +4,8 @@ import SwiftUI
 struct CameraPreview: UIViewRepresentable {
     let session: AVCaptureSession
     var onFocus: (CGPoint) -> Void
+    var onZoom: (CGFloat) -> Void
+    var currentZoom: CGFloat
 
     func makeUIView(context: Context) -> PreviewView {
         let view = PreviewView()
@@ -12,24 +14,36 @@ struct CameraPreview: UIViewRepresentable {
 
         let tap = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.tapped(_:)))
         view.addGestureRecognizer(tap)
+
+        let pinch = UIPinchGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.pinched(_:)))
+        pinch.delegate = context.coordinator
+        view.addGestureRecognizer(pinch)
+
         context.coordinator.previewView = view
+        context.coordinator.currentZoom = currentZoom
         return view
     }
 
     func updateUIView(_ uiView: PreviewView, context: Context) {
         uiView.videoPreviewLayer.session = session
+        context.coordinator.currentZoom = currentZoom
     }
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(onFocus: onFocus)
+        Coordinator(onFocus: onFocus, onZoom: onZoom, currentZoom: currentZoom)
     }
 
-    final class Coordinator: NSObject {
+    final class Coordinator: NSObject, UIGestureRecognizerDelegate {
         let onFocus: (CGPoint) -> Void
+        let onZoom: (CGFloat) -> Void
         weak var previewView: PreviewView?
+        var currentZoom: CGFloat
+        private var pinchStartZoom: CGFloat = 1
 
-        init(onFocus: @escaping (CGPoint) -> Void) {
+        init(onFocus: @escaping (CGPoint) -> Void, onZoom: @escaping (CGFloat) -> Void, currentZoom: CGFloat) {
             self.onFocus = onFocus
+            self.onZoom = onZoom
+            self.currentZoom = currentZoom
         }
 
         @objc func tapped(_ recognizer: UITapGestureRecognizer) {
@@ -38,6 +52,22 @@ struct CameraPreview: UIViewRepresentable {
             let devicePoint = view.videoPreviewLayer.captureDevicePointConverted(fromLayerPoint: point)
             onFocus(devicePoint)
             view.showFocusIndicator(at: point)
+        }
+
+        @objc func pinched(_ recognizer: UIPinchGestureRecognizer) {
+            switch recognizer.state {
+            case .began:
+                pinchStartZoom = currentZoom
+            case .changed, .ended:
+                let requested = pinchStartZoom * recognizer.scale
+                onZoom(requested)
+            default:
+                break
+            }
+        }
+
+        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+            true
         }
     }
 }
